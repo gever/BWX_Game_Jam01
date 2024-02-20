bc37ee50b03c3e3a5be030529a6c260329e6fa5c
Russel Simmons <russ@rsimmons.org>
2024-02-19 20:58:28 -0800

    Restore and improve player animation

--- src/entity_player.py
+++ src/entity_player.py
@@ -13,19 +13,23 @@ def load():
 class PlayerAssets:
     def __init__(self):
         spritesheet = pygame.image.load('../gfx/Sprout Lands - Sprites - premium pack/Characters/Basic Charakter Spritesheet.png').convert_alpha()
-        self.sprites = []
+        sprites = []
         for i in range(0, 4):
             frame = spritesheet.subsurface((16*(3*i + 1), 16, 16, 16))
-            self.sprites.append(frame)
+            sprites.append(frame)
+        self.anims = {
+            'standing': sprites[0:1],
+            'walking': sprites[2:4],
+        }
         self.anchor = (7, 12)
 
 # It is convenient to create an instance of the player in each level, rather than "moving" the player between levels
 class Player(BaseEntity):
     def __init__(self, space, initial_pos):
-        self.anchor = assets.anchor
-        self.sprites = assets.sprites
-        self.framenum = 0
         self.space = space
+
+        self.current_anim = 'standing'
+        self.anim_phase = 0
 
         self.body = pymunk.Body(1, float('inf'))
         self.body.position = initial_pos
@@ -35,10 +39,12 @@ class Player(BaseEntity):
         space.add(self.body, self.shape)
 
     def get_render_info(self):
+        anim = assets.anims[self.current_anim]
+        frame = int(self.anim_phase * len(anim)) % len(anim)
         return {
-            'sprite': self.sprites[self.framenum],
+            'sprite': anim[frame],
             'pos': self.body.position,
-            'anchor': self.anchor,
+            'anchor': assets.anchor,
         }
 
     def handle_input(self, keys, events, dt):
@@ -61,7 +67,10 @@ class Player(BaseEntity):
         self.apply_force_to_achieve_velocity(desired_velo, PLAYER_MOVEMENT_MAX_ACCEL)
 
         # # update player sprite frame
-        # if desired_velo.is_zero():
-        #     self.player.anim_standing()
-        # else:
-        #     self.player.anim_walking(dt)
+        if desired_velo.is_zero():
+            self.current_anim = 'standing'
+            self.anim_phase = 0
+        else:
+            self.current_anim = 'walking'
+            PLAYER_WALKING_ANIM_TIME = 0.25
+            self.anim_phase += dt / PLAYER_WALKING_ANIM_TIME
