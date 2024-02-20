a18868a4ad6edaba226e03b515feec070f97e527
Beck Kogan <beck.kogan@sfbrightworks.org>
2024-02-20 15:21:53 -0800

    Added 3rd Demo Level

---
+++ src/level_lava_and_key.py
@@ -0,0 +1,6 @@
+from audio import get_audio
+from level_base import BaseLevel
+
+class DungeonLevel_LavaAndKey(BaseLevel):
+    def __init__(self):
+        super().__init__('lava and key.tmx')
--- src/levels_loader.py
+++ src/levels_loader.py
@@ -1,8 +1,10 @@
 from level_dungeon import DungeonLevel
 from level_dungeon_2 import DungeonLevel2
+from level_lava_and_key import DungeonLevel_LavaAndKey
 
 def load_levels():
     return [
         DungeonLevel(),
         DungeonLevel2(),
+        DungeonLevel_LavaAndKey(),
     ]
