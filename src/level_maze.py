db737aca546c2b57286a19d2c8780482ceae99ce
Beck Kogan <beck.kogan@sfbrightworks.org>
2024-02-21 11:57:58 -0800

    Added a invisible maze level

---
+++ src/level_maze.py
@@ -0,0 +1,9 @@
+from audio import get_audio
+from level_base import BaseLevel
+from entity_skull_monster import SkullMonster
+from entity_slime_monster import SlimeMonster
+from collision_types import *
+
+class DungeonLevel_Maze(BaseLevel):
+    def __init__(self):
+        super().__init__('Maze.tmx')
--- src/levels_loader.py
+++ src/levels_loader.py
@@ -2,6 +2,7 @@ from level_dungeon import DungeonLevel
 from level_dungeon_2 import DungeonLevel2
 from level_lava_and_key import DungeonLevel_LavaAndKey
 from level_puzzle_room import DungeonLevel_PuzzleRoom
+from level_maze import DungeonLevel_Maze
 
 def load_levels():
     return [
@@ -9,4 +10,5 @@ def load_levels():
         DungeonLevel2(),
         DungeonLevel_LavaAndKey(),
         DungeonLevel_PuzzleRoom(),
+        DungeonLevel_Maze(),
     ]
