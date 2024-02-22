c422bb1e7a1ebf41a83cb77e5452563cf4ceb9b7
Theo Linehan <theo.linehan@sfbrightworks.org>
2024-02-22 15:10:16 -0800

    Add new maps

---
+++ maps/cave_crawler.tsx
@@ -0,0 +1,134 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="cave_crawler" tilewidth="16" tileheight="16" spacing="1" tilecount="225" columns="15">
+ <image source="../../../Downloads/cave_crawler.png" width="256" height="256"/>
+ <tile id="0">
+  <properties>
+   <property name="kills you" type="bool" value="true"/>
+   <property name="passable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="kills you" type="bool" value="true"/>
+   <property name="passable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="kills you" type="bool" value="true"/>
+   <property name="passable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="kills you" type="bool" value="true"/>
+   <property name="passable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="kills you" type="bool" value="true"/>
+   <property name="passable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="kills you" type="bool" value="true"/>
+   <property name="passable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="kills you" type="bool" value="true"/>
+   <property name="passable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="kills you" type="bool" value="true"/>
+   <property name="passable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="kills you" type="bool" value="true"/>
+   <property name="passable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="kills you" type="bool" value="true"/>
+   <property name="passable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="kills you" type="bool" value="true"/>
+   <property name="passable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="passable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="passable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="passable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="passable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="75">
+  <properties>
+   <property name="passable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="76">
+  <properties>
+   <property name="passable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="77">
+  <properties>
+   <property name="passable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="78">
+  <properties>
+   <property name="passable" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="90">
+  <properties>
+   <property name="kill you" type="bool" value="true"/>
+   <property name="passable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="91">
+  <properties>
+   <property name="kill you" type="bool" value="true"/>
+   <property name="passable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="105">
+  <properties>
+   <property name="kill you" type="bool" value="true"/>
+   <property name="passable" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="106">
+  <properties>
+   <property name="kill you" type="bool" value="true"/>
+   <property name="passable" type="bool" value="false"/>
+  </properties>
+ </tile>
+</tileset>
