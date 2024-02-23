bd53d4741d5e357ef28261338ed3f415cc52cd1e
Russel Simmons <russ@rsimmons.org>
2024-02-22 19:42:33 -0800

    Add button+bridge level, reorder levels

--- src/levels_loader.py
+++ src/levels_loader.py
@@ -3,12 +3,14 @@ from level_dungeon_2 import LevelDungeon2
 from level_lava_and_key import LevelLavaAndKey
 from level_puzzle_room import LevelPuzzleRoom
 from level_maze import LevelMaze
+from level_button_and_bridge import LevelButtonAndBridge
 
 def load_levels():
     return [
+        LevelButtonAndBridge(),
+        LevelLavaAndKey(),
         LevelDungeon(),
         LevelDungeon2(),
-        LevelLavaAndKey(),
         LevelPuzzleRoom(),
         LevelMaze(),
     ]
