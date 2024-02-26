be0b134257950844505b02a6f7e593bd9cd76638
Russel Simmons <russ@rsimmons.org>
2024-02-26 15:13:53 -0800

    Fix "kills you" tile property

--- maps/cave_crawler_v2.tsx
+++ maps/cave_crawler_v2.tsx
@@ -1,4 +1,19 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="cave_crawler_v2" tilewidth="16" tileheight="16" spacing="1" tilecount="225" columns="15">
  <image source="../gfx/cave_crawler_v2.png" width="256" height="256"/>
+ <tile id="45">
+  <properties>
+   <property name="kills you" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="kills you" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="47">
+  <properties>
+   <property name="kills you" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
