2aaf9b1c96d97453d2f5ed00310cbd33071b3655
Theo Linehan <theo.linehan@sfbrightworks.org>
2024-02-26 14:45:26 -0800

    latest map and tile edits (nothing major)

---
+++ maps/cave_crawler_v2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="cave_crawler_v2" tilewidth="16" tileheight="16" spacing="1" tilecount="225" columns="15">
+ <image source="../../../Downloads/cave_crawler_v2.png" width="256" height="256"/>
+</tileset>
