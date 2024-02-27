82784557d942fba961ff75baa0a2036557c45f54
Russel Simmons <russ@rsimmons.org>
2024-02-27 13:10:32 -0800

    Allow maps to specify level subclass

--- src/level_dungeon.py
+++ src/level_dungeon.py
@@ -7,9 +7,9 @@ from entity_skull_monster import SkullMonster
 from collision_types import *
 from player_state import player_state
 
-class LevelDungeon(BaseLevel):
-    def __init__(self):
-        super().__init__('dungeon_map.tmx')
+class Level(BaseLevel):
+    def reset(self):
+        super().reset()
 
         self.COLLISION_TYPE_BLUE_LIGHT = get_new_collision_type_id()
         for tile in self.visible_tiles:
@@ -25,9 +25,4 @@ class LevelDungeon(BaseLevel):
     def handle_blue_light_collision(self, arbiter, space, data):
         get_audio().play_sfx('rasp')
         player_state.apply_damage()
-        for shape in arbiter.shapes:
-            if hasattr(shape.body, 'entity_name'):
-                print(shape.body.entity_name)
-            else:
-                print('no entity_name')
         return True
--- src/levels_loader.py
+++ src/levels_loader.py
@@ -1,5 +1,7 @@
 import os
 
+from map import TiledMap
+
 from level_base import BaseLevel
 
 def load_levels():
@@ -11,11 +13,21 @@ def load_levels():
     sorted_map_fns.sort()
 
     levels = []
-    for fn in sorted_map_fns:
-        print('loading map:', fn)
-        full_fn = os.path.join('../maps', fn)
+    for map_fn in sorted_map_fns:
+        print('loading map:', map_fn)
 
-        levels.append(BaseLevel(full_fn))
+        # load one copy of the map just to look at its properties,
+        # to see if we want to actually load it with a subclass.
+        # this seems inefficient but is not a big deal.
+        temp_map = TiledMap(os.path.join('../maps', map_fn))
+        subclass_name = temp_map.tmx_data.properties.get('subclass')
+        if subclass_name:
+            imp = __import__(subclass_name, globals(), locals(), ['Level'], 0)
+            Level = getattr(imp, 'Level')
+            print('map %r has subclass %r' % (map_fn, subclass_name))
+            levels.append(Level(map_fn))
+        else:
+            levels.append(BaseLevel(map_fn))
 
     return levels
 
