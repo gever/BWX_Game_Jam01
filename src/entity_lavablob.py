479e6609af0fdb301f8d3c947cc0892853d0ceba
Dash Grantham <dashel.grantham@sfbrightworks.org>
2024-02-27 17:59:21 -0800

    Lava blobs turn to stone!!!

--- src/entity_lavablob.py
+++ src/entity_lavablob.py
@@ -10,7 +10,9 @@ def load():
 class LavaBlobAssets:
     def __init__(self):
         spritesheet = pygame.image.load('../gfx/Lava Blob.png').convert_alpha()
+        paused_spritesheet = pygame.image.load('../gfx/Stone Lava Blob.png').convert_alpha()
         self.sprite = spritesheet.subsurface((17, 20, 16, 12))
+        self.pausedsprite = paused_spritesheet.subsurface((17, 20, 16, 12))
         self.anchor = (8, 14)
 
 class LavaBlob(BaseEntity):
@@ -18,32 +20,41 @@ class LavaBlob(BaseEntity):
         super().__init__(level, initial_pos)
         self.chasing = False
         self.time_until_death = 2
+        self.paused = False
 
     def get_render_info(self):
         return {
-            'sprite': assets.sprite,
+            'sprite': assets.pausedsprite if self.paused else assets.sprite,
             'pos': self.body.position,
             'anchor': assets.anchor,
         }
     
     def handle_entity_collision(self, other_entity):
         if other_entity.is_player():
-            other_entity.remove()
+            if not self.paused:
+                other_entity.remove()
 
     def act(self,dt):
         MAX_SPEED = 85
         MOVEMENT_STRENGTH = 15
         player = self.get_nearest_player()
         
-        if player: 
+        if player:
             dist = player.body.position.get_distance(self.body.position)
             if dist < 75:
                self.chasing = True
 
-            if self.chasing:
-                self.move_towards_player(MAX_SPEED, MOVEMENT_STRENGTH)
-                self.time_until_death -= dt
-                if self.time_until_death <0:
-                    self.remove()
+            if self.chasing == True:
+                if self.paused == False:
+                    self.move_towards_player(MAX_SPEED, MOVEMENT_STRENGTH)
+                    self.time_until_death -= dt
+                    if self.time_until_death <0:
+                        self.paused = True
+                        
+                else:
+                    self.chasing = False
+                    self.body.velocity = (0,0)
+            else:
+                self.body.velocity = (0,0)
 
 
