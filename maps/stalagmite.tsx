3cba59bc1c76c087cab870930b29d4231c7136b1
Tatsuo Sciortino <tatsuo.sciortino@sfbrightworks.org>
2024-03-05 15:20:01 -0800

    hopefully fixes art team get your act together

--- maps/stalagmite.tsx
+++ maps/stalagmite.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="stalagmite" tilewidth="16" tileheight="16" spacing="1" tilecount="3" columns="1">
- <image source="../../stalagmite.png" width="32" height="64"/>
+ <image source="../gfx/stalagmite.png" width="32" height="64"/>
 </tileset>
