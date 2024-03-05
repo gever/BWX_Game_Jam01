c9565caae45e241fbc68887f53f69cea2063f024
Dash Grantham <dashel.grantham@sfbrightworks.org>
2024-03-05 13:58:13 -0800

    Bat animation

--- src/entity_bat_monster.py
+++ src/entity_bat_monster.py
@@ -10,21 +10,28 @@ def load():
 class BatMonsterAssets:
     def __init__(self):
         spritesheet = pygame.image.load('../gfx/bat_sprite.png').convert_alpha()
-        self.sprite = spritesheet.subsurface((0, 0, 16, 9))
+        self.sprite = spritesheet.subsurface((0, 0, 16, 12))
         self.anchor = (8, 8)
+        self.spritelist = []
+        for i in range (0,2):
+            frame = spritesheet.subsurface(((17*i), 0, 16, 12))
+            self.spritelist.append(frame)
 
 class BatMonster(BaseEntity):
     def __init__(self, level, initial_pos):
         super().__init__(level, initial_pos)
+        self.timer = 0
 
     def get_render_info(self):
+        frame = int(self.timer) % len(assets.spritelist)
         return {
-            'sprite': assets.sprite,
+            'sprite': assets.spritelist[frame],
             'pos': self.body.position,
             'anchor': assets.anchor,
         }
 
     def act(self, dt):
+        self.timer += dt*7
         MAX_SPEED = 85
         MOVEMENT_STRENGTH = 15
         self.move_towards_player(MAX_SPEED, MOVEMENT_STRENGTH)
