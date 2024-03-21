4304281c89f0a1f51276980f5c19856682eeb9d9
Beck Kogan <beck.kogan@sfbrightworks.org>
2024-03-21 15:01:38 -0700

    added glow to exits

--- src/entity_player.py
+++ src/entity_player.py
@@ -1,5 +1,6 @@
 import pygame
 import pymunk
+import time
 
 from config import *
 from entity_base import BaseEntity
@@ -14,8 +15,10 @@ class PlayerAssets:
     def __init__(self):
         spritesheet = pygame.image.load('../gfx/player_sprite.png').convert_alpha()
         swim_spritesheet = pygame.image.load('../gfx/Swimming_player_sprite.png').convert_alpha()
+        death_player_spritesheet = pygame.image.load('../gfx/Dead_Player.png').convert_alpha()
         sprites = []
         swim_sprites = []
+        dead_sprites = []
         for i in range(0, 4):
             frame = spritesheet.subsurface(((17*i + 1), 1, 16, 20))
             sprites.append(frame)
@@ -30,6 +33,12 @@ class PlayerAssets:
             'swim_left': swim_sprites[0:1],
             'swim_right': swim_sprites[1:2],
         }
+        for i in range(0, 1):
+            dead_frames = death_player_spritesheet.subsurface((0, 1, 24, 16))
+            dead_sprites.append(dead_frames)
+        self.dead_anims = {
+            'dead': dead_sprites[0:1],
+        }
         self.anchor = (7, 16)
 
 # It is convenient to create an instance of the player in each level, rather than "moving" the player between levels:
@@ -39,37 +48,43 @@ class Player(BaseEntity):
 
         self.current_anim = 'right'
         self.swim_anim = 'swim_right'
+        self.dead_anim = 'dead'
         self.anim_phase = 0
         self.stamina = MAX_STAMINA
         self.desired_velo = Vec2(0,0)
         self.in_water = False
+        self.dead = False
+        self.death_counter = 0
 
     def get_render_info(self):
         anim = assets.anims[self.current_anim]
         swim_anim = assets.swim_anims[self.swim_anim]
+        dead_anim = assets.dead_anims[self.dead_anim]
         frame = int(self.anim_phase * len(anim)) % len(anim)
         swim_frame = int(self.anim_phase * len(swim_anim)) % len(swim_anim)
+        dead_frame = int(self.anim_phase * len(dead_anim)) % len(dead_anim)
         return {
-            'sprite': anim[frame] if self.in_water == False else (swim_anim[swim_frame]),
+            'sprite': anim[frame] if self.in_water == False and self.dead == False else (dead_anim[dead_frame]) if self.dead else(swim_anim[swim_frame]),
             'pos': self.body.position,
             'anchor': assets.anchor,
         }
 
     def handle_input(self, keys, events, dt):
-        # determine desired player velocity based on keyboard input
-        self.desired_velo = Vec2()
-        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            self.desired_velo += Vec2(-1, 0)
-            self.current_anim = 'left'
-            self.swim_anim = 'swim_left'
-        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            self.desired_velo += Vec2(1, 0)
-            self.current_anim = 'right'
-            self.swim_anim = 'swim_right'
-        if keys[pygame.K_UP] or keys[pygame.K_w]:
-            self.desired_velo += Vec2(0, -1)
-        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-            self.desired_velo += Vec2(0, 1)
+        if self.dead == False:
+            # determine desired player velocity based on keyboard input
+            self.desired_velo = Vec2()
+            if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+                self.desired_velo += Vec2(-1, 0)
+                self.current_anim = 'left'
+                self.swim_anim = 'swim_left'
+            if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+                self.desired_velo += Vec2(1, 0)
+                self.current_anim = 'right'
+                self.swim_anim = 'swim_right'
+            if keys[pygame.K_UP] or keys[pygame.K_w]:
+                self.desired_velo += Vec2(0, -1)
+            if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+                self.desired_velo += Vec2(0, 1)
 
         # normalize and scale desired velocity
         if not self.desired_velo.is_zero():
@@ -104,8 +119,18 @@ class Player(BaseEntity):
     def is_player(self):
         return True
 
+    def die_if_tile_kills_you(self):
+        tile_props = self.get_current_tile_props()
+        if tile_props and tile_props.get('kills you'):
+            self.dead = True
+
     def act(self, dt):
         self.die_if_tile_kills_you()
+        if self.dead:
+            self.desired_velo = Vec2(0,0)
+            self.death_counter += 1
+            if self.death_counter >= 60:
+                self.remove()
 
     def get_lighting(self):
         return 100
